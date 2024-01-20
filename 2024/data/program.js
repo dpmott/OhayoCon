68014bcad7e85030437b1dc00f32f1be6ac29d29
David Mott <dpmott@gmail.com>
2024-01-20 16:16:33 -0500

    Updating initial content

--- 2024/data/program.js
+++ 2024/data/program.js
@@ -1,1 +1,1 @@
-var program = [{"id":"1","title":"NYE Party","date":"2024-01-19","time":"23:00","mins":"50","desc":"Opening Ceremonies","loc":["Panel Room"],"tags":["Convention"],"people":[{"name":"David Mott","id":"1"},{"name":"Michelle Mussoni","id":"2"}]}];
+var program = [{"id":"1","title":"Opening Ceremonies","date":"2024-01-19","time":"23:00","mins":"50","desc":"Opening Ceremonies","loc":["Panel Room"],"tags":["Convention"],"people":[{"name":"David Mott","id":"1"},{"name":"Michelle Mussoni","id":"2"}]}];
