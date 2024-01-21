8bda785330a6e560c7710c6c3399ad66266c95ca
David Mott <dpmott@gmail.com>
2024-01-20 21:55:17 -0500

    Adding Friday programming

--- 2024/util2/genPocketSchedule.sh
+++
@@ -1,4 +0,0 @@
-#!bash -e
-node pocketSchedule.js  --locations "Main Bridge" Engineering Astrophysics "The Lab" "Motley Kids" Communications Promenade "Vic's Place" --exclude "Starship Astraeus" "Myst Brigade Bridge"
-
-
